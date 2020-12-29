86949430cec1dcbf19e25abe32ebd80288a83347
Hugues Delamare <bluekikiller74@gmail.com>
2020-12-29 20:25:09 +0100

    Minor update
    -Minor update for stock variable
    -Adding re library

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 links = []
 
+#Boucle pour parcourir les
 for i in range(1, 4):
     url = 'http://books.toscrape.com/catalogue/category/books/fantasy_19/page-' + str(i) + '.html'
     response = requests.get(url)
@@ -20,6 +22,8 @@ for i in range(1, 4):
             if response2.ok:
                 soup2 = BeautifulSoup(response2.text, 'html.parser')
                 article_link = url_article
+                article_stock_selector = str(soup2.find('table', {'class': 'table table-striped'}).select('td')[5].text)
+                article_stock = re.findall(r'\d+', article_stock_selector)[0]
                 article_description = soup2.find('article', {'class': 'product_page'}).select('p')[3].text
                 article_upc = soup2.find('table', {'class': 'table table-striped'}).select('td')[0].text
                 article_price_including_tax = soup2.find('table', {'class': 'table table-striped'}).select('td')[3].text
@@ -29,4 +33,4 @@ for i in range(1, 4):
                 article_picture_selector = soup2.find('div', {'class': 'item active'}).find('img')
                 article_picture = article_picture_selector['src']
                 article_title = soup2.find('div', {'class': 'col-sm-6 product_main'}).select('h1')[0].text
-                print(article_picture)
+                print(article_stock)
