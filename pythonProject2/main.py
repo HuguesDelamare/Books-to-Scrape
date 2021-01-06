1507ce47cd2ec06482f6fba61b6c5d720b23ab8b
Hugues Delamare <bluekikiller74@gmail.com>
2021-01-06 05:40:38 +0100

    Update 06/01
    -Minor changes on variables for book infos
    -Added a function to check the array containing books datas

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import re
 
 
-
 def get_books_infos_from_category(url):
         response = requests.get(url)
         if response.ok:
@@ -32,7 +31,19 @@ def get_books_infos(books):
             article_price_excluding_tax = soup2.find('table', {'class': 'table table-striped'}).select('td')[
                 2].text
             article_category = soup2.find('ul', {'class': 'breadcrumb'}).select('li')[2].text
-            article_review = ""
+            article_review_selector = soup2.find('div', {'class': 'col-sm-6 product_main'}).select('p')[2]['class'][1]
+            if article_review_selector == "One":
+                article_review = int(1)
+            elif article_review_selector == "Two":
+                article_review = int(2)
+            elif article_review_selector == "Three":
+                article_review = int(3)
+            elif article_review_selector == "Four":
+                article_review = int(4)
+            elif article_review_selector == "Five":
+                article_review = int(5)
+            else:
+                article_review = int(0)
             article_picture_selector = soup2.find('div', {'class': 'item active'}).find('img')
             article_picture = article_picture_selector['src']
             article_title = soup2.find('div', {'class': 'col-sm-6 product_main'}).select('h1')[0].text
@@ -41,17 +52,18 @@ def get_books_infos(books):
                           article_price_excluding_tax, article_stock, article_description, article_category,
                           article_review, article_image]
             all_book_datas.append(book_datas)
-    print(all_book_datas)
+    check_result(all_book_datas)
+
 def find_next_page(soup, url):
     next_page_selector = soup.find('section').find('li', {'class': 'next'}).select('a')
     if next_page_selector == " " or next_page_selector is None:
         print("No next page found")
+        return
     else:
         next_page_href = next_page_selector[0]['href']
         url_split = url.rsplit("/", 1)
         next_page_url = url_split[0] + str("/" + next_page_href)
         return next_page_url
-
 
 ### Fonction pour trouver toutes les catégories présentes sur le site   ###
 def get_books_categories():
@@ -62,11 +74,14 @@ def get_books_categories():
         soup1 = BeautifulSoup(response_main_page_url.text, 'html.parser')
         #for category in soup1.find('ul', {'class': 'nav nav-list'}).find('ul').select('li'):
             #category_link = "http://books.toscrape.com/" + category.a['href']
-        get_books_infos_from_category('http://books.toscrape.com/catalogue/category/books/romance_8/index.html')
+        get_books_infos_from_category('http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html')
 
-def write_csv_file():
-    print('bonjour')
+def check_result(array):
+    for result in array:
+        print(result[2])
+        print(result[8])
 
+### DEMARAGE DU CODE ###
 get_books_categories()
 
 
